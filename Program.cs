b7ec61cb48791f9b3cc71bb8f899499f3436a104
Wand3rvogel <voronin.andrey@gmail.com>
2018-04-21 13:23:42 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharpExample
+{
+    class Program
+    {
+        public static void ColoredConsoleWrite(ConsoleColor color, string text)
+        {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = originalColor;
+        }
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello World!");
+            Console.ResetColor();
+            Console.WriteLine("Normal");
+            ColoredConsoleWrite(ConsoleColor.Red, "Test");
+        }
+    }
+}
